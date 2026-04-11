aeae4b799da665e6a6ceb0941fb40e3241ab8df7
mario-lome <esamlome@gmail.com>
2026-04-11 19:22:34 +0000

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,21 +1,22 @@
 {
   "name": "EnglishCoachAdzo",
   "short_name": "CoachAdzo",
-  "description": "Application d'apprentissage de l'anglais pour enfants, pros et autodidactes",
-  "start_url": "/index.html",
+  "description": "Apprentissage de l'anglais - Enfants & Pro",
+  "start_url": "/?source=pwa",
+  "scope": "/",
   "display": "standalone",
   "background_color": "#f8f9fa",
   "theme_color": "#4361ee",
   "orientation": "portrait",
   "icons": [
     {
-      "src": "assets/icons/icon-192.png",
+      "src": "/assets/icons/icon-192.png",
       "sizes": "192x192",
       "type": "image/png",
       "purpose": "any maskable"
     },
     {
-      "src": "assets/icons/icon-512.png",
+      "src": "/assets/icons/icon-512.png",
       "sizes": "512x512",
       "type": "image/png",
       "purpose": "any maskable"
